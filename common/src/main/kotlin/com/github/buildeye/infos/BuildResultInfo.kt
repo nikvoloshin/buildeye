7dab03476db0ecfb9d8a79528511e8d2dc2d4149
Nikita Voloshin <nik.volosh@yandex.ru>
2019-04-19 06:26:50 +0300

    Store message and stack trace instead of Throwable in BuildFailInfo.
    In order to correctly serialize to/deserialize from JSON.

--- common/src/main/kotlin/com/github/buildeye/infos/BuildResultInfo.kt
+++ common/src/main/kotlin/com/github/buildeye/infos/BuildResultInfo.kt
@@ -1,5 +1,5 @@
 package com.github.buildeye.infos
 
 sealed class BuildResultInfo
-data class BuildFailInfo(val action: String, val failure: Throwable) : BuildResultInfo()
+data class BuildFailInfo(val action: String, val failureMessage: String, val stackTrace: String) : BuildResultInfo()
 data class BuildSuccessInfo(val action: String) : BuildResultInfo()
