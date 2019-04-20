840aafa1d17327ac8c395f9f0ec6cbd5508df132
Nikita Voloshin <nik.volosh@yandex.ru>
2019-04-20 18:57:07 +0300

    Add utility methods for creating FailureInfo from Throwable

--- gradle-plugin/src/main/kotlin/com/github/buildeye/utils/Utils.kt
+++ gradle-plugin/src/main/kotlin/com/github/buildeye/utils/Utils.kt
@@ -1,5 +1,17 @@
 package com.github.buildeye.utils
 
+import com.github.buildeye.infos.FailureInfo
+import org.gradle.internal.impldep.org.apache.commons.lang.exception.ExceptionUtils
 import java.util.concurrent.TimeUnit
 
 fun millisTime() = TimeUnit.NANOSECONDS.toMillis(System.nanoTime())
+
+fun createFailureInfo(failure: Throwable) = FailureInfo(
+        failure.message ?: "",
+        ExceptionUtils.getFullStackTrace(failure)
+)
+
+fun createFailureInfoOfNullable(failure: Throwable?) = when (failure) {
+    null -> null
+    else -> createFailureInfo(failure)
+}
