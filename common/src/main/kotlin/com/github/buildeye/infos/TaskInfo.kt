0a42dd7da972c2aeba27408eaefe03ed9328f423
Nikita Voloshin <nik.volosh@yandex.ru>
2019-04-26 13:29:34 +0300

    Change task duration type from Duration to Long
    Since Info data classes are going to be used only for
    (de)serialization, it's better to use just Long

--- common/src/main/kotlin/com/github/buildeye/infos/TaskInfo.kt
+++ common/src/main/kotlin/com/github/buildeye/infos/TaskInfo.kt
@@ -1,11 +1,9 @@
 package com.github.buildeye.infos
 
-import java.time.Duration
-
 data class TaskInfo(
         val name: String,
         val startedTimestamp: Long,
         val finishedTimestamp: Long,
         val taskStateInfo: TaskStateInfo,
-        val duration: Duration = Duration.ofMillis(finishedTimestamp - startedTimestamp)
+        val duration: Long = finishedTimestamp - startedTimestamp
 )
