b8d7f1dcab1ab09ef6b4ae06c50c13afaa8f2482
Nikita Voloshin <nik.volosh@yandex.ru>
2019-04-29 08:15:24 +0300

    Add extension methods to Map and Remove from Iterable

--- gradle-plugin/src/main/kotlin/com/github/buildeye/utils/CollectionsUtils.kt
+++ gradle-plugin/src/main/kotlin/com/github/buildeye/utils/CollectionsUtils.kt
@@ -3,6 +3,14 @@ package com.github.buildeye.utils
 import kotlinx.coroutines.async
 import kotlinx.coroutines.coroutineScope
 
-suspend fun <T, R> Iterable<T>.parallelMap(transform: suspend (T) -> R): List<R> = coroutineScope {
-    map { async { transform(it) } }.map { it.await() }
+suspend fun <K, V, R> Map<K, V>.parallelMapValues(transform: suspend (Map.Entry<K, V>) -> R): Map<K, R> = coroutineScope {
+    mapValues { async { transform(it) } }.mapValues { it.value.await() }
+}
+
+fun <K, V> Map<K, V?>.filterValuesNotNull(): Map<K, V> {
+    val destination = LinkedHashMap<K, V>()
+    for (element in this) {
+        element.value?.also { destination[element.key] = it }
+    }
+    return destination
 }
