a0b528f92ff091de5df5d308acff6eaa0fe5d894
Nikita Voloshin <nik.volosh@yandex.ru>
2019-12-22 19:27:32 +0300

    Refactor tests

--- web-application/backend/test/com/github/buildeye/storage/BuildInfosDatabaseTest.kt
+++ web-application/backend/test/com/github/buildeye/storage/BuildInfosDatabaseTest.kt
@@ -2,19 +2,60 @@ package com.github.buildeye.storage
 
 import com.github.buildeye.infos.*
 import org.jetbrains.squash.dialects.h2.H2Connection
-import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
 import java.util.*
-import kotlin.NoSuchElementException
-import kotlin.collections.ArrayList
 import kotlin.test.assertEquals
 import kotlin.test.assertFailsWith
-import kotlin.test.assertFalse
-import kotlin.test.assertNull
+import kotlin.test.assertTrue
 
 internal class BuildInfosDatabaseTest {
-    private val storage: BuildInfosStorage = BuildInfosDatabase(H2Connection.createMemoryConnection())
+    private lateinit var storage: BuildInfosDatabase
+
+    @BeforeEach
+    fun setUp() {
+        storage = BuildInfosDatabase(H2Connection.createMemoryConnection())
+    }
+
+    @AfterEach
+    fun tearDown() {
+        storage.close()
+    }
+
+    @Test
+    fun fetchBuildInfo() {
+        val id = storage.insertBuildInfo(buildInfo)
+        assertEquals(buildInfo, storage.fetchBuildInfo(id).buildInfo)
+    }
+
+    @Test
+    fun fetchBuildInfoNull() {
+        assertFailsWith<NoSuchElementException> { storage.fetchBuildInfo(0).buildInfo }
+    }
+
+    @Test
+    fun fetchAllBuildInfosEmpty() {
+        assertTrue(storage.fetchAllBuildInfos().isEmpty())
+    }
+
+    @Test
+    fun fetchAllBuildInfos() {
+        val id1 = storage.insertBuildInfo(buildInfo)
+        val id2 = storage.insertBuildInfo(buildInfo)
+        val id3 = storage.insertBuildInfo(buildInfo)
+        val id4 = storage.insertBuildInfo(buildInfo)
+        assertEquals(4, storage.fetchAllBuildInfos().size)
+
+        assertEquals(listOf(id1, id2, id3, id4), storage.fetchAllBuildInfos().map { it.id })
+
+        val buildInfo = storage.fetchAllBuildInfos().last()
+        assertEquals(id4 to this.buildInfo, buildInfo.id to buildInfo.buildInfo)
+    }
+
+    @Test
+    fun close() = storage.close()
+
     private val project = "test"
     private val switchesInfo = SwitchesInfo(
             isBuildCacheEnabled = true,
@@ -45,40 +86,4 @@ internal class BuildInfosDatabaseTest {
     private val buildResultInfo = BuildResultInfo(BuildResultInfo.Action.BUILD, failure = FailureInfo("none", "none"))
     private val buildInfo = BuildInfo(project, switchesInfo, infrastructureInfo, executionInfo, buildResultInfo)
 
-    @Test
-    fun fetchBuildInfo(){
-        val id = storage.insertBuildInfo(buildInfo)
-        assertEquals(buildInfo, storage.fetchBuildInfo(id).buildInfo)
-    }
-
-    @Test
-    fun fetchBuildInfoNull(){
-        assertFailsWith<NoSuchElementException> {
-            assertEquals(buildInfo, storage.fetchBuildInfo(0).buildInfo)
-        }
-    }
-
-    @Test
-    fun fetchAllBuildInfosEmpty(){
-        assertFalse(storage.fetchAllBuildInfos().isNotEmpty())
-    }
-
-    @Test
-    fun fetchAllBuildInfos(){
-        val id1 = storage.insertBuildInfo(buildInfo)
-        val id2 = storage.insertBuildInfo(buildInfo)
-        val id3 = storage.insertBuildInfo(buildInfo)
-        val id4 = storage.insertBuildInfo(buildInfo)
-        assertEquals(4, ArrayList(storage.fetchAllBuildInfos()).size)
-
-        assertEquals(listOf(id1, id2, id3, id4), storage.fetchAllBuildInfos().map(IndexedBuildInfo::id))
-
-        val indexedBuildInfo1 = storage.fetchAllBuildInfos().last()
-        assertEquals(Pair(id4, buildInfo), Pair(indexedBuildInfo1.id, indexedBuildInfo1.buildInfo))
-    }
-
-    @Test
-    fun close() {
-        (storage as BuildInfosDatabase).close()
-    }
 }
