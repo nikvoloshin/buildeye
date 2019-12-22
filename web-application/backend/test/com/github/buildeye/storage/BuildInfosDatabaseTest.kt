ca903f0f25c9305ca5e8d56128cb5632583ec68d
Paul Khuzhin <poul1298@gmail.com>
2019-12-22 18:59:32 +0300

    Add few db test

--- web-application/backend/test/com/github/buildeye/storage/BuildInfosDatabaseTest.kt
+++ web-application/backend/test/com/github/buildeye/storage/BuildInfosDatabaseTest.kt
@@ -2,9 +2,16 @@ package com.github.buildeye.storage
 
 import com.github.buildeye.infos.*
 import org.jetbrains.squash.dialects.h2.H2Connection
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertAll
 import java.util.*
+import kotlin.NoSuchElementException
+import kotlin.collections.ArrayList
 import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+import kotlin.test.assertFalse
+import kotlin.test.assertNull
 
 internal class BuildInfosDatabaseTest {
     private val storage: BuildInfosStorage = BuildInfosDatabase(H2Connection.createMemoryConnection())
@@ -39,12 +46,39 @@ internal class BuildInfosDatabaseTest {
     private val buildInfo = BuildInfo(project, switchesInfo, infrastructureInfo, executionInfo, buildResultInfo)
 
     @Test
-    fun test(){
+    fun fetchBuildInfo(){
         val id = storage.insertBuildInfo(buildInfo)
         assertEquals(buildInfo, storage.fetchBuildInfo(id).buildInfo)
     }
 
     @Test
+    fun fetchBuildInfoNull(){
+        assertFailsWith<NoSuchElementException> {
+            assertEquals(buildInfo, storage.fetchBuildInfo(0).buildInfo)
+        }
+    }
+
+    @Test
+    fun fetchAllBuildInfosEmpty(){
+        assertFalse(storage.fetchAllBuildInfos().isNotEmpty())
+    }
+
+    @Test
+    fun fetchAllBuildInfos(){
+        val id1 = storage.insertBuildInfo(buildInfo)
+        val id2 = storage.insertBuildInfo(buildInfo)
+        val id3 = storage.insertBuildInfo(buildInfo)
+        val id4 = storage.insertBuildInfo(buildInfo)
+        assertEquals(4, ArrayList(storage.fetchAllBuildInfos()).size)
+
+        assertEquals(listOf(id1, id2, id3, id4), storage.fetchAllBuildInfos().map(IndexedBuildInfo::id))
+
+        val indexedBuildInfo1 = storage.fetchAllBuildInfos().last()
+        assertEquals(Pair(id4, buildInfo), Pair(indexedBuildInfo1.id, indexedBuildInfo1.buildInfo))
+    }
+
+    @Test
     fun close() {
+        (storage as BuildInfosDatabase).close()
     }
 }
